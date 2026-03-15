1b2f2063c48d998141a0fe9332bd42f3ac842ace
arsalanans47 <arsalanans8377@gmail.com>
2026-03-16 04:06:41 +0530

    added common crud classes for basic crud operations

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,7 +1,11 @@
+const AirportRepository = require('./airport-repository');
+const CrudRepository = require('./crud-repository');
 const FlightRepository = require('./flight-repository');
 
 module.exports = {
   CityRepository : require('./city-repository'),
   FlightRepository : require('./flight-repository'),
   AirplaneRepository : require('./airplane-repository'),
+  AirportRepository : require('./airport-repository'),
+  CrudRepository : require('./crud-repository')
 } 
